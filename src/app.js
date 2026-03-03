f8af8014cef57847bea4b5abb7dbce11cee5a6f6
avfitnes <ashokvarma9636@gmail.com>
2026-03-03 18:34:10 +0530

    cors error solve

--- src/app.js
+++ src/app.js
@@ -23,13 +23,35 @@ import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "http://127.0.0.1:5173",
+  "https://mindgymbook.ductfabrication.in",
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("Origin blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+      "Origin",
+    ],
     exposedHeaders: ["X-Is-Preview"],
+    optionsSuccessStatus: 200,
   }),
 );
 app.use(cookieParser());
